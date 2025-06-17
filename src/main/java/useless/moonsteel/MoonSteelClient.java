4aad0f393aae69489e070b24e26cc6416fac66fc
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2025-06-17 12:33:03 -0500

    Update MoonSteelClient.java

--- src/main/java/useless/moonsteel/MoonSteelClient.java
+++ src/main/java/useless/moonsteel/MoonSteelClient.java
@@ -1,16 +1,28 @@
 package useless.moonsteel;
 
 import net.minecraft.client.entity.particle.ParticleDispatcher;
+import net.minecraft.client.render.texture.stitcher.AtlasStitcher;
+import net.minecraft.client.render.texture.stitcher.TextureRegistry;
 import net.minecraft.client.sound.SoundRepository;
 import turniplabs.halplibe.util.ClientStartEntrypoint;
 import useless.moonsteel.fx.ParticleMagicSmoke;
 import useless.moonsteel.fx.ParticleStar;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 import static useless.moonsteel.MoonSteel.MOD_ID;
 
 public class MoonSteelClient implements ClientStartEntrypoint {
 	@Override
 	public void beforeClientStart() {
+		for (final AtlasStitcher stitcher : TextureRegistry.stitcherMap.values()) {
+			try {
+				TextureRegistry.initializeAllFiles(MOD_ID, stitcher, true);
+			} catch (URISyntaxException | IOException e) {
+				MoonSteel.LOGGER.error("Failed to initialize texture files!", e);
+			}
+		}
 		SoundRepository.registerNamespace(MOD_ID);
 	}
 
